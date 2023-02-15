f78154670a671bb7db0324849e6a843d8a9d01ac
Vallejos Jose Luis <joseluisvallejos1995@gmail.com>
2023-02-15 15:25:04 -0300

    feat: :zap: Form validations was added.

--- frontend/src/app/components/form/form.component.ts
+++ frontend/src/app/components/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-form',
@@ -7,4 +9,44 @@ import { Component } from '@angular/core';
 })
 export class FormComponent {
 
-}
+  formLogin = new FormGroup({
+    firstName: new FormControl('',[
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(10),
+      Validators.pattern('[a-zA-Z]*')
+    ]),
+    lastName: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(10),
+      Validators.pattern('[a-zA-Z]*')
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.pattern('[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$')
+
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(12)
+    ]),
+  });
+
+  constructor() {
+    this.formLogin.valueChanges.pipe(debounceTime(350)).subscribe(value => {
+      console.log(value);
+    });
+  };
+
+
+  send(): any {
+    console.log(this.formLogin.value);
+
+  };
+  }
+
+
+
---
+++ frontend/src/app/shared/interfaces/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    name: string;
+    lastName: string;
+    email: string;
+    password: string;
+    password2: string;
+}
+
+// Path: frontend/src/app/shared/interfaces/user.ts
